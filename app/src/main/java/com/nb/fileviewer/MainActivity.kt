22dc174f03d344caad6d90e3c759afc8d99fd28e
nitishb29 <nitishbatham@gmail.com>
2026-05-04 00:40:34 +0530

    FileViewer Initial UI changes.

--- app/src/main/java/com/nb/fileviewer/MainActivity.kt
+++ app/src/main/java/com/nb/fileviewer/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.nb.fileviewer
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.nb.fileviewer.ui.theme.FileViewerTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
